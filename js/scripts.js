a64a2154c2e33709389b2699e6b093ea728a928a
Akigawa <sonoskeakigawa@gmail.com>
2018-10-18 15:49:59 -0700

    ???????

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,58 @@
+var disiterator = 0;
+
 $(document).ready(function() {
+              $(".meattext").hide();
+    $("button#meat").addClass("disappear");
+
   $("button#green").click(function() {
     $("body").removeClass();
     $("body").addClass("green-background");
-          $("button#green").addClass("disappear");
-  });
+    $("button#green").addClass("disappear");
+    disiterator++;
+    checkiterator();
 
-  $("button#yellow").click(function() {
+    });
+    $("button#yellow").click(function() {
       $("body").removeClass();
       $("body").addClass("yellow-background");
       $("button#yellow").addClass("disappear");
       $("button#yellow").removeClass("button#yellow");
-  });
+      disiterator++;
+      checkiterator();
+      });
 
-  $("button#red").click(function() {
-    $("body").removeClass();
-    $("body").addClass("red-background");
-    $("button#red").addClass("disappear");
-  });
-  $("button#blippo").click(function() {
-    $("body").removeClass();
-    $("body").addClass("blippo-background");
-    $("button#blippo").addClass("disappear");
-  });
-});
+      $("button#red").click(function() {
+        $("body").removeClass();
+        $("body").addClass("red-background");
+        $("button#red").addClass("disappear");
+        disiterator++;
+        checkiterator();
+        });
+
+        $("button#blippo").click(function() {
+          $("body").removeClass();
+          $("body").addClass("blippo-background");
+          $("button#blippo").addClass("disappear");
+          disiterator++;
+          checkiterator();
+          });
+
+          $("button#meat").click(function() {
+            $("body").removeClass();
+            $("body").addClass("meat");
+            $("button#meat").addClass("disappear");
+            $("button#meat").removeClass("show");
+            $(".meattext").show();
+
+            });
+            function checkiterator()
+            {
+              console.log(disiterator);
+              if (disiterator === 4)
+              {
+                $("button#meat").addClass("show");
+
+                disiterator++;
+              }
+            }
+          });
